12ff0f47660e955b149db5d677c5926925609b48
br <brsurin@gmail.com>
2014-04-05 23:38:23 +0530

    first commit

--- LispInterpreter/main.c
+++ LispInterpreter/main.c
@@ -8,11 +8,21 @@
 
 #include <stdio.h>
 
+static char input[1024];
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    puts("Lispy Interprter v 0.01. ");
+    puts("Press C-c to exit\n");
+
+    while (1) {
+        fputs("lipsy>", stdout);
+        fgets(input, 1024, stdin);
+        printf("Okay. You entered %s", input);
+        
+    }
+    
     return 0;
 }
 
