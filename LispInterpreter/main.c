38c73b1588b9ebe7f2c437a53ced2ce1877b34a9
br <brsurin@gmail.com>
2014-04-05 23:26:24 +0530

    Initial Commit

---
+++ LispInterpreter/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LispInterpreter
+//
+//  Created by bhuthesh r on 05/04/14.
+//  Copyright (c) 2014 bhuthesh r. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
