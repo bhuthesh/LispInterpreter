cd4447174a68f10e142ec92096f8180052fc3f91
br <brsurin@gmail.com>
2014-04-08 18:55:14 +0530

    added parsing using mpc

--- LispInterpreter/parsing.c
+++ LispInterpreter/parsing.c
@@ -9,7 +9,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef __WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+// realine function for windows
+char* reeadline(char* prompt) {
+    fputs("lispy>", stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+void add_history(char* line){
+    
+}
+
+#else
+
 #include <editline/readline.h>
+
+#endif
 
 
 
