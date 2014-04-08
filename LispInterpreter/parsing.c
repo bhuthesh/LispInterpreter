6e95c95ba957ff7708ecf2744704d698955b84d7
br <brsurin@gmail.com>
2014-04-08 18:56:17 +0530

    added parsing.c

--- LispInterpreter/parsing.c
+++ LispInterpreter/parsing.c
@@ -9,15 +9,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "mpc.h"
+
+#define INPUT_BUFFER_SIZE 2048
 #ifdef __WIN32
 #include <string.h>
 
-static char buffer[2048];
+static char buffer[INPUT_BUFFER_SIZE];
 
 // realine function for windows
 char* reeadline(char* prompt) {
     fputs("lispy>", stdout);
-    fgets(buffer, 2048, stdin);
+    fgets(buffer, INPUT_BUFFER_SIZE, stdin);
     char* cpy = malloc(strlen(buffer) + 1);
     strcpy(cpy, buffer);
     cpy[strlen(cpy) - 1] = '\0';
@@ -39,15 +42,43 @@ void add_history(char* line){
 int main(int argc, const char * argv[])
 {
 
+    // create some parsers
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expression = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+    
+    mpca_lang(MPC_LANG_DEFAULT,
+              "                                             \
+                number : /-?[0-9]+/ ;                       \
+                operator : '+' | '-' | '*' | '/' ;          \
+                expr : <number> | '('<operator> <expr>+')'; \
+                lispy : /^/ <operator> <expr>+ /$/ ;        \
+              ",
+              Number, Operator, Expression, Lispy);
+    
     puts("Lispy Interprter v 0.01. ");
     puts("Press C-c to exit\n");
 
     while (1) {
+        // parse the input
         char* input = readline("lispy>");
         add_history(input);
-        printf("you entered %s\n", input);
+        //printf("you entered %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input);
     }
+    
+    mpc_cleanup(4, Number, Operator, Expression, Lispy);
     
     return 0;
 }
